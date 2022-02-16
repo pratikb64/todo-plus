ece817d04421a0a586168e05e4212fa1a58d3bae
Pratik Badhe <badhepd@gmail.com>
2022-02-16 16:47:02 +0530

    base url

--- src/configs/Constants.ts
+++ src/configs/Constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
-	BASE_URL: 'http://localhost:5000'
-	// BASE_URL: 'https://todopluss.herokuapp.com'
+	// BASE_URL: 'http://localhost:5000'
+	BASE_URL: 'https://todopluss.herokuapp.com'
 }
 
 export default CONSTANTS
